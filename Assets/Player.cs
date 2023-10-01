07310df93fb4d9ff13468cbf51c87b0d5a4f4031
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2023-10-01 13:04:49 +0300

    Football mechanics

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Input.mousePosition);
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        //transform.position = mousePos;
+        GetComponent<Rigidbody2D>().MovePosition(mousePos);
+    }
+}
