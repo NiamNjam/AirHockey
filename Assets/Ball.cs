b073f460c30dd4531eaa58a0189bf15d7f0d9b6d
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2023-10-01 13:52:08 +0300

    Finish

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Ball : MonoBehaviour
 {
+    int PlayerScore = 0;
+    int EnemyScore = 0;
+
+    public TMP_Text playerScore;
+    public TMP_Text enemyScore;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +25,31 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        
         if (collision.gameObject.name.Contains("Gates"))
         {
             print("Goal!!!");
             transform.position = Vector3.zero;
             GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         }
+        
+
 
         if (collision.gameObject.name.Contains("Player Gates"))
         {
-            // give score to player
+            //int score = 0;
+            PlayerScore += 1;
+            playerScore.text = PlayerScore.ToString();
+
+            
         }
 
         if (collision.gameObject.name.Contains("Enemy Gates"))
         {
             // give score to enemy
+            EnemyScore += 1;
+            enemyScore.text = EnemyScore.ToString();
+
         }
     }
 }
